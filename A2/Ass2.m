46336c35eadf6c02be8f40fc1bbe48a71d764e7d
mathieupelle <mathieu.pelle7@gmail.com>
2021-11-01 23:39:50 +0100

    Made RK4_KdV function. Started e).

--- A2/Ass2.m
+++ A2/Ass2.m
@@ -184,9 +184,10 @@ colorbar
 %%  Question (c) - Solving the KdV equation
 
 saving_hist = 1;
+conservation = 0;
 
-Ni_lst = [20, 40, 60, 100, 300, 1000] + 1;
 Ni_lst = 2.^(3:9) + 1;
+Ni_lst = 31;
 finalL2norm = zeros(length(Ni_lst),1);
 for n=1:length(Ni_lst)
     Ni = Ni_lst(n);
@@ -195,61 +196,9 @@ for n=1:length(Ni_lst)
     c = 5;
     x0 = 0;
 
-    x_end = 2*pi;
-    x_start = -2*pi;
-    scaling = (x_end-x_start)/(2*pi);
-    dx = (x_end-x_start)/(Ni+1);
-    x = x_start:dx:x_end-dx;
-
-    u_init = 0.5*c*sech(0.5*sqrt(c).*(x-c*0-x0)).^2;
-    dt = 2.82/(3*(Ni+1)*max(abs(u_init))+(Ni+1)^3/8);
-    time = 0:dt:0.1; %2s? How long to run for?
-
-
-    %u_init = sech(x);
-    u = u_init';
-
-    u_hist = zeros(length(x),length(time));
-    u_hist(:,1) = u_init;
-
-    u_ana = zeros(length(x),length(time));
-    u_ana(:,1) = u_init;
-    L2norm = zeros(length(time),1);
-    L2norm(1) = sqrt(trapz((u_hist(:,1) - u_ana(:,1)).^2, x));
-
-%     D = get_FourierDiffMatrix(Ni+1,false)/2;
-%     D3 = D*D*D;
-    for t=1:length(time)-1
-
-        % Diff. matrix
-%         K1 = -6*u'*D*u - D3*u;
-%         K2 = -6*(u'+dt*K1/2)*D*(u+dt*K1/2) - D3*(u+dt*K1/2);
-%         K3 = -6*(u'+dt*K2/2)*D*(u+dt*K2/2) - D3*(u+dt*K2/2);
-%         K4 = -6*(u'+dt*K3/2)*D*(u+dt*K3/2) - D3*(u+dt*K3/2);
-
-        %FFT
-        K1 = KdV(u, scaling);
-        K2 = KdV(u+dt*K1/2, scaling);
-        K3 = KdV(u+dt*K2/2, scaling);
-        K4 = KdV(u+dt*K3, scaling);
-        u = u + dt/6*(K1+2*K2+2*K3+K4);
-        
-        if saving_hist == 1
-            u_hist(:,t+1) = u;
-            u_ana(:,t+1) = 0.5*c*sech(0.5*sqrt(c)*(x-c*time(t+1)-x0)).^2;
-               
-            L2norm(t+1) = sqrt(abs(trapz((u - u_ana(:,t+1)).^2, x))); 
-        else
-            if t == length(time)-1
-                u_hist(:,end) = u;
-                u_ana(:,end) = 0.5*c*sech(0.5*sqrt(c)*(x-c*time(end)-x0)).^2;
-            end
-        end
-
-    end
+    [u_hist, u_ana, errors, x, time, quant] = RK4_KdV(Ni, c, x0, saving_hist, conservation);
     
-    finalL2norm(n,1 ) = sqrt(abs(trapz((u - u_ana(:,end)).^2, x)));
-
+    finalL2norm(n,1 ) = sqrt(abs(trapz((u_hist(:,end) - u_ana(:,end)).^2, x)));
 
     if n==length(Ni_lst)
         if saving_hist ==1
@@ -260,7 +209,6 @@ for n=1:length(Ni_lst)
 %         xlabel('x')
 %         ylabel('t')
 %         zlabel('$\overline{u}(x,t)$')
-%         
 %         
 %         figure('Name', 'Soliton')
 %         surf(X,T,u_ana')
@@ -275,7 +223,7 @@ for n=1:length(Ni_lst)
 %         zlabel('$|\overline{u}(x,t) - u(x,t)|$')
 
             figure('Name', 'L2 norm in time')
-            plot(time, L2norm)
+            plot(time, errors.L2norm)
             xlabel('Time')
             ylabel('L2')
             grid on
@@ -289,7 +237,6 @@ for n=1:length(Ni_lst)
         legend
         grid on
     end
-
 end
 
 figure('Name', 'Convergence')
@@ -297,116 +244,112 @@ loglog(Ni_lst, finalL2norm)
 grid on
 xlabel('N')
 ylabel('$||u-\mathcal{I}_Nu||_{L2}$')
-%%
 
-if saving_hist ==1
-    figure
-    hold on
-    for i = 1:10:length(time)
-        plot(x,u_hist(:,i), '-r')
-        hold on
-        plot(x,u_ana(:,i), '--ok')
-        pause(0.001)
-        clf
-    end
-end
+% if saving_hist == 1
+%     figure
+%     hold on
+%     for i = 1:10:length(time)
+%         plot(x,u_hist(:,i), '-r')
+%         hold on
+%         plot(x,u_ana(:,i), '--ok')
+%         pause(0.005)
+%         clf
+%     end
+% end
 
 %% Question (d)
 
-c_lst = [0.25, 0.5, 1];
-T = 100;
-[norm2, normInf, M, M_ana, V, V_ana, E, E_ana] = deal(zeros(length(c_lst), T));
-for i=1:length(c_lst)
+saving_hist = 1;
+conservation = 1;
 
+c_lst = [0.25, 0.5, 1];
+leg = {'c = 0.25', 'c = 0.5', 'c = 1.0'};
+f1 = figure;
+f2 = figure;
+f3 = figure;
+f4 = figure;
+f5 = figure;
+for i=1:length(c_lst)
+    
     Ni = 55;
     x0 = 0;
     c = c_lst(i);
-
-    x_end = pi;
-    x_start = -pi;
-    dx = (x_end-x_start)/(Ni+1);
-    x = x_start:dx:x_end-dx;
-
-    u_init = 0.5*c*sech(0.5*sqrt(c).*(x-c*0-x0)).^2;
-    dt = 2.82/(3*(Ni+1)*max(abs(u_init))+(Ni+1)^3/8);
-    time = 0:dt:dt*T;
-
-
-    %u_init = sech(x);
-    u = u_init';
-
-    u_hist = zeros(length(x),length(time));
-    u_hist(:,1) = u_init;
-
-    u_ana = zeros(length(x),length(time));
-    u_ana(:,1) = u_init;
     
+    [~, ~, errors, ~, time, quant] = RK4_KdV(Ni, c, x0, saving_hist, conservation);
     
-    % D = get_FourierDiffMatrix(Ni+1,false)/2;
-    % D3 = D*D*D;
-    for t=1:length(time)-1
-
-        % Diff. matrix
-    %     K1 = -6*u'*D*u - D3*u;
-    %     K2 = -6*(u'+dt*K1/2)*D*(u+dt*K1/2) - D3*(u+dt*K1/2);
-    %     K3 = -6*(u'+dt*K2/2)*D*(u+dt*K2/2) - D3*(u+dt*K2/2);
-    %     K4 = -6*(u'+dt*K3/2)*D*(u+dt*K3/2) - D3*(u+dt*K3/2);
-
-
-        %FFT
-        K1 = KdV(u);
-        K2 = KdV(u+dt*K1/2);
-        K3 = KdV(u+dt*K2/2);
-        K4 = KdV(u+dt*K3);
-        u = u_hist(:,t) + dt/6*(K1+2*K2+2*K3+K4);
-        u_hist(:,t+1) = u;
-        u_ana(:,t+1) = 0.5*c*sech(0.5*sqrt(c).*(x-c*time(t+1)-x0)).^2;
-
-        L2norm(t+1) = sqrt(trapz((u - u_ana(:,t+1)).^2, x));
-        
-        norm2(i,t+1) = norm(u - u_ana(:,t+1) ,2);
-        normInf(i,t+1) = norm(u - u_ana(:,t+1) , Inf);
-        M(i,t+1) = trapz(u, x);
-        M_ana(i,t+1) = trapz(u_ana(:,t+1), x);
-        V(i,t+1) = trapz(u.^2, x);
-        V_ana(i,t+1) = trapz(u_ana(:,t+1).^2, x);
-        %E(i,t) = trapz(0.5*ux.^2 - u^3, x); 
-        %E_ana(i,t) = trapz(0.5*ux_ana.^2 - u^3, x);
-
-    end
-    
-end
-
-
-lab = {'$||u-\mathcal{I}_Nu||_2$', '$||u-\mathcal{I}_Nu||_\infty$',... 
-       'Mass (M)', 'Momentum (V)', 'Energy (E)'};
-vars = {norm2, normInf, M, V, E};
-vars_ana = {M_ana, V_ana, E_ana};
-leg = {'c = 0.25', 'c = 0.5', 'c = 0.1'};
-for i=1:length(vars)-1
-    figure
-    if i > 2
-        colors = {'r', 'g', 'b'};
-        for j=1:length(c_lst)
-            y = vars{i};
-            y_ana = vars_ana{i-2};
-            plot(time, y(j,:), colors{j}, 'DisplayName', leg{j})
-            hold on
-            plot(time, y_ana(j,:), append('--', colors{j}))
-        end
-    else
-        plot(time, vars{i})
-        legend(leg)
-    end
-    
+    set(0, 'CurrentFigure', f1)
+    loglog(time, errors.norm2)
     grid on
+    hold on
     xlabel('Time')
-    ylabel(lab{i})
-    legend
+    ylabel('$||u-\mathcal{I}_Nu||_2$')
+    legend(leg, 'Location', 'Best')
+    
+    set(0, 'CurrentFigure', f2)
+    loglog(time, errors.normInf)
+    grid on
+    hold on
+    xlabel('Time')
+    ylabel('$||u-\mathcal{I}_Nu||_\infty$')
+    legend(leg, 'Location', 'Best')
+
+    set(0, 'CurrentFigure', f3)
+    err = abs(quant.M - quant.M_ana)./abs(quant.M_ana);
+    loglog(time, err)
+    grid on
+    hold on
+    xlabel('Time')
+    ylabel('Mass (M)')
+    legend(leg, 'Location', 'Best')
+    
+    set(0, 'CurrentFigure', f4)
+    err = abs(quant.V - quant.V_ana)./abs(quant.V_ana);
+    loglog(time, err)
+    grid on
+    hold on
+    xlabel('Time')
+    ylabel('Momentum (V)')
+    legend(leg, 'Location', 'Best')
+    
+    
+    set(0, 'CurrentFigure', f5)
+    err = abs(quant.E - quant.E_ana)./abs(quant.E_ana);
+    loglog(time, err)
+    grid on
+    hold on
+    xlabel('Time')
+    ylabel('Energy (E)')
+    legend(leg, 'Location', 'Best')
     
 end
-
 
 %%
 
+saving_hist = 1;
+conservation = 0;
+
+Ni = 31;
+c = 5;
+x0 = 0;
+
+[u_hist, u_ana, ~, ~, time, ~] = RK4_KdV(Ni, c, x0, saving_hist, conservation);
+
+cn = fft(u_hist)/(Ni+1);
+cn = fftshift(cn); % Re-order accordingly
+k = 0:(Ni+1)/2-1;
+cn = cn((Ni+1)/2+1:end,:);
+
+cn_ana = fft(u_ana)/(Ni+1);
+cn_ana = fftshift(cn_ana); % Re-order accordingly
+cn_ana = cn_ana((Ni+1)/2+1:end,:);
+
+figure
+for t=1:5
+    plot(time, abs(cn(t,:)-cn_ana(t,:)), 'DisplayName', num2str(time(t)))
+    hold on
+end
+grid on
+xlabel('k')
+ylabel('$c_k$')
+legend
 
---
+++ A2/RK4_KdV.m
@@ -0,0 +1,97 @@
+function [u_hist, u_ana, errors, x, time, quant] = RK4_KdV(Ni, c, x0, saving_hist, conservation)
+    
+    % Spatial discretisation
+    x_end = 2*pi; %left xlim
+    x_start = -2*pi; %right xlim
+    scaling = (x_end-x_start)/(2*pi); %scaling factor
+    dx = (x_end-x_start)/(Ni+1); %spatial element width
+    x = x_start:dx:x_end-dx; %x vector
+    
+    % Temporal discretisation and IC
+    u_init = 0.5*c*sech(0.5*sqrt(c).*(x-c*0-x0)).^2; %initial guess
+    dt = 2.82/(3*(Ni+1)*max(abs(u_init))+(Ni+1)^3/8); %time step from RK4 stability
+    time = 0:dt:5; %time vector
+
+    
+    % Allocating storage
+    [u_hist, u_ana] = deal(zeros(length(x), length(time))); %spectral and analytic solutions  
+    u = u_init';
+    u_hist(:,1) = u_init;
+    u_ana(:,1) = u_init;
+    
+    [L2norm, norm2, normInf, M, M_ana, V, V_ana, E, E_ana] = deal(zeros(length(time), 1));
+    
+    L2norm(1) = sqrt(trapz((u_hist(:,1) - u_ana(:,1)).^2, x));
+    
+    if conservation == 1
+        % Errors
+        norm2(1,1) = norm(u - u_ana(:,1) ,2); %2 Norm
+        normInf(1,1) = norm(u - u_ana(:,1) , Inf); %Infinity Norm
+        
+        %Conservation quantities
+        M(1,1) = trapz(u, x); %Mass (spectral)
+        M_ana(1,1) = trapz(u_ana(:,1), x); %Mass (analytical)
+        V(1,1) = trapz(u.^2, x); %Momentum (spectral)
+        V_ana(1,1) = trapz(u_ana(:,1).^2, x); %Momentum (analytical)
+        D = get_FourierDiffMatrix(Ni+1,false)/scaling; %Differentiation matrix
+        ux = D*u; %Derivative (spectral)
+        ux_ana = D*u_ana(:,1); %Derivative (analytical)
+        E(1,1) = trapz(0.5*ux.^2 - u.^3, x); %Momentum (spectral)
+        E_ana(1,1) = trapz(0.5*ux_ana.^2 - u.^3, x); %Momentum (analytical)
+
+    end
+
+%     D = get_FourierDiffMatrix(Ni+1,false)/2;
+%     D3 = D*D*D;
+    for t=1:length(time)-1
+
+        % Diff. matrix Runge-Kutta
+%         K1 = -6*u'*D*u - D3*u;
+%         K2 = -6*(u'+dt*K1/2)*D*(u+dt*K1/2) - D3*(u+dt*K1/2);
+%         K3 = -6*(u'+dt*K2/2)*D*(u+dt*K2/2) - D3*(u+dt*K2/2);
+%         K4 = -6*(u'+dt*K3/2)*D*(u+dt*K3/2) - D3*(u+dt*K3/2);
+
+        %FFT Runge-Kutta
+        K1 = KdV(u, scaling);
+        K2 = KdV(u+dt*K1/2, scaling);
+        K3 = KdV(u+dt*K2/2, scaling);
+        K4 = KdV(u+dt*K3, scaling);
+        u = u + dt/6*(K1+2*K2+2*K3+K4);
+        
+        if saving_hist == 1 %saving each time step
+            u_hist(:,t+1) = u; %spectral
+            u_ana(:,t+1) = 0.5*c*sech(0.5*sqrt(c)*(x-c*time(t+1)-x0)).^2; %analytic
+            L2norm(t+1) = sqrt(abs(trapz((u - u_ana(:,t+1)).^2, x)));  %L2 norm
+            
+            if conservation == 1
+                norm2(t+1) = norm(u - u_ana(:,t+1) ,2);
+                normInf(t+1) = norm(u - u_ana(:,t+1) , Inf);
+                M(t+1) = trapz(u, x);
+                M_ana(t+1) = trapz(u_ana(:,t+1), x);
+                V(t+1) = trapz(u.^2, x);
+                V_ana(t+1) = trapz(u_ana(:,t+1).^2, x);
+                ux = D*u;
+                ux_ana = D*u_ana(:,t+1);
+                E(t+1) = trapz(0.5*ux.^2 - u.^3, x);
+                E_ana(t+1) = trapz(0.5*ux_ana.^2 - u.^3, x);
+            end
+        else 
+            if t == length(time)-1 %saving last time step only
+                u_hist(:,end) = u; %spectral
+                u_ana(:,end) = 0.5*c*sech(0.5*sqrt(c)*(x-c*time(end)-x0)).^2; %analytic
+            end
+        end
+
+    end
+    
+    errors.L2norm = L2norm;
+    errors.norm2 = norm2;
+    errors.normInf = normInf;
+    quant.M = M;
+    quant.V = V;
+    quant.E = E;
+    quant.M_ana = M_ana;
+    quant.V_ana = V_ana;
+    quant.E_ana = E_ana;
+    
+end  
